0711ec8440c82dd83bee076155f62965d84057f3
Matt Raible <matt.raible@okta.com>
2018-06-13 00:35:00 -0600

    Change back to 'jasmine' framework

--- crypto-pwa/test/protractor.conf.js
+++ crypto-pwa/test/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   },
   directConnect: true,
   baseUrl: 'http://localhost:8100/',
-  framework: 'jasmine2',
+  framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
